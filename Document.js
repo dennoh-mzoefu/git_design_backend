a39eca33eb73bbaf02bba18c3e610766f2d6c78d
Denis Gichana <denisgichana162@gmail.com>
2022-10-25 22:24:38 +0300

    chat module

--- Document.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require("mongoose");
-const Document = new Schema({
-  _id: String,
-  author: String,
-  time: String,
-  message: String,
-});
-module.exports = model("Document", Document);
--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-const mongoose = require("mongoose");
-// async function connectMongo() {
-//   await mongoose.connect("mongodb://127.0.0.1/google-docs", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true,
-//   });
-// }
-// connectMongo();
-const CONNECTION__URL =
-  "mongodb+srv://deno1804:deno1804@cluster0.76w4jft.mongodb.net/?retryWrites=true&w=majority";
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 
-mongoose
-  .connect(CONNECTION__URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log(`Db connected`);
-  })
-  .catch((error) => console.log(error));
-const Document = require("./Document.js");
+const app = express();
 
-const io = require("socket.io")(3001, {
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
@@ -27,23 +16,20 @@ const io = require("socket.io")(3001, {
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
-  socket.on("get-document", async (documentId) => {
-    const document = findOrCreateDocument(documentId);
-    socket.join(documentId);
-    socket.emit("load-document", document.data);
-    socket.on("send-changes", (delta) => {
-      socket.broadcast.to(documentId).emit("receive-changes", delta);
-    });
-    socket.on("save-document", async (data) => {
-      await Document.findByIdAndUpdate(documentId, { data });
-    });
+  console.log(`User connected ${socket.id}`);
+
+  socket.on("join-room", (data) => {
+    socket.join(data);
+    console.log(`user ${socket.id} joined room ${data}`);
+  });
+  socket.on("send-message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive-message", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
   });
 });
-const defaultValue = "";
-async function findOrCreateDocument(id) {
-  if (id == null) return;
-  const document = await Document.findById(id);
-  if (document) return document;
-  return await Document.create({ _id: id, data: defaultValue });
-}
+server.listen(3001, () => {
+  console.log("Server Running");
+});
