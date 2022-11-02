c72821beb053c05b4a695b7018fa9810adddaf84
Denis Gichana <denisgichana162@gmail.com>
2022-11-02 23:39:54 +0300

    notification progress

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const chatController = require("../controllers/chatController.js");
-// router.get("/", chatController.getAllChats);
+
 router.post("/chatSave", chatController.chatSave);
 router.get("/", chatController.getAllChats);
-router.get(
-  "/senderNotification",
-  chatController.getAgetNotificationSenderllChats
-);
-router.get("/receiverNotification", chatController.getNotificationReceiver);
-router.post("/createNotification", chatController.createNotification);
+// router.get(
+//   "/senderNotification",
+//   chatController.getAgetNotificationSenderllChats
+// );
+// router.get("/receiverNotification", chatController.getNotificationReceiver);
+// router.post("/createNotification", chatController.createNotification);
 
 module.exports = router;
