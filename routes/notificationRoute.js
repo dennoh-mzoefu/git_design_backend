aeaf564bf23ee027537b9747e37621c0a1a73206
Denis Gichana <denisgichana162@gmail.com>
2022-11-10 22:47:40 +0300

    WIP

--- routes/notificationRoute.js
+++ routes/notificationRoute.js
@@ -3,7 +3,7 @@ const notificationController = require("../controllers/notificationCotrollers.js
 
 router.post("/", notificationController.createNotification);
 // router.get("/", notificationController.getAllChats);
-router.post("/notify", notificationController.getNotifications);
+router.get("/notify/:receiver", notificationController.getNotifications);
 router.post("/accept", notificationController.acceptNotifications);
 
 module.exports = router;
