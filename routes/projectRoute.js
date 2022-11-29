392bd9993a149398256080d00fa106d6bf79908a
Denis Gichana <denisgichana162@gmail.com>
2022-11-29 12:31:11 +0300

    add routes

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const projectController = require("../controllers/projectController.js");
 
 router.get("/:ownerName", projectController.getProjects);
-router.get("/:projectName", projectController.getProject);
+router.get("/view/:projectName", projectController.getProject);
 router.post("/:id", projectController.updateProject);
 router.post("/", projectController.createProject);
 router.delete("/:id", projectController.deleteProject);
