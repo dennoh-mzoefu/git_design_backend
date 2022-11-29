4dde736b1630acc2d00d2bce0414f78531e12ff9
Denis Gichana <denisgichana162@gmail.com>
2022-11-29 12:28:53 +0300

    add chat

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3007",
     methods: ["GET", "POST"],
   },
 });
