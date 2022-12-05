0e9facad2f330a182a6adc2263cb5b5ddd1273a8
Denis Gichana <denisgichana162@gmail.com>
2022-12-05 16:50:16 +0300

    wip

--- routes/activityLogRoute.js
+++ routes/activityLogRoute.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const activityLogController = require("../controllers/activityLogController.js");
-// router.post("/create", designFileController.createFigmaFile);
-// router.post("/:id", designFileController.updateDesignFile);
-// router.get("/:id", designFileController.getDesignFile);
+router.post("/", activityLogController.saveActivityLog);
+
 router.get("/", activityLogController.getActivityLogs);
 
 module.exports = router;
